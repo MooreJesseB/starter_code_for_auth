6a66aba0673e885c443383f1a96a5c3701683cec
JesseMoore <vergobret@gmail.com>
2014-08-01 10:32:54 -0700

    Got authentication working and most routes functional

--- models/user.js
+++ models/user.js
@@ -7,10 +7,11 @@ module.exports = function (sequelize, DataTypes){
    var User = sequelize.define('user', {
      username: { 
         type: DataTypes.STRING, 
+        allowNull: false,
         unique: true, 
         validate: {
           len: [6, 30],
-          }
+        }
     },
     password: {
         type:DataTypes.STRING,
@@ -20,43 +21,40 @@ module.exports = function (sequelize, DataTypes){
       }
     },
     
-  {
-    classMethods: {
-      associate: function(db){
-        User.hasMany(db.post);
-      },
-      encryptPass: function(password) {
-        var hash = bcrypt.hashSync(password, salt);
-        return hash;
-      }, 
-      comparePass: function(userpass, dbpass) {
-        // don't salt twice when you compare....watch out for this
-          return bcrypt.compareSync(userpass, dbpass);  
-      },
-      createNewUser:function(username, password, err, success ) {
-        if(password.length < 6) {
-          err({message: "Password should be more than six characters"});
-        }
-        else{
-        User.create({
+    { classMethods: {
+        associate: function(db){
+          User.hasMany(db.post);
+        },
+        encryptPass: function(password) {
+          var hash = bcrypt.hashSync(password, salt);
+          return hash;
+        }, 
+        comparePass: function(userpass, dbpass) {
+          // don't salt twice when you compare....watch out for this
+            return bcrypt.compareSync(userpass, dbpass);  
+        },
+        createNewUser:function(username, password, err, success ) {
+          if(password.length < 6) {
+            err({message: "Password should be more than six characters"});
+          }
+          else{
+          User.create({
             username: username,
             password: User.encryptPass(password)
           }).error(function(error) {
             console.log(error);
             if(error.username){
               err({message: 'Your username should be at least 6 characters long', username: username});
-            }
-            else{
+            } else{
               err({message: 'An account with that username already exists', username: username});
-              }
+            }
           }).success(function(user) {
-            success({message: 'Account created, please log in now'});
-          });
-        }
-      },
-    }
-  } //close classMethods outer 
-
+              success({message: 'Account created, please log in now'});
+            });
+          }
+        },
+      }
+    } //close classMethods outer 
   ); // close define user
   
   passport.use(new passportLocal.Strategy( {
@@ -65,7 +63,6 @@ module.exports = function (sequelize, DataTypes){
       passReqToCallback: true
     }, function(req, username, password, done) {
       // find a user in the DB
-      console.log("Running find...")
       User.find({
         where: {
           username: username
@@ -88,7 +85,6 @@ module.exports = function (sequelize, DataTypes){
         done(null, user);
       });
     }));
-
   return User;
 }; // close User function
 
